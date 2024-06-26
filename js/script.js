3553584980a3572f12a574744d822355c78b55d4
Rivio98 <rivaldogjoni1998@gmail.com>
2024-06-26 17:49:17 +0200

    [UPD] Added button action event

--- js/script.js
+++ js/script.js
@@ -3,5 +3,11 @@ const genera = document.getElementById('genera')
 
 // Button ID annulla event
 const annulla = document.getElementById('annulla')
+// Button genera action event
+genera.addEventListener('click', function () {
+    const nameSurname = document.getElementById('nameSurname').value
+    const km = document.getElementById('km').value
+    const ageSelect = document.getElementById('age-select').value
+})
 
-
+//creo una costante per i km  che l'utente deve fare, ai quali assegno il valore restituito da un form
